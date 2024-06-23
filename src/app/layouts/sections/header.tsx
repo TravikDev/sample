50e66b253e714a3a51a71fd10dc20efbb84b4be3
Travik <travikdev@gmail.com>
2024-06-23 07:35:24 -0700

    msw ready

--- src/app/layouts/sections/header.tsx
+++ src/app/layouts/sections/header.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+
+
+
 export const Header = () => {
+
+    const [user, setUser] = useState<[{ id: number, name: string }]>([{ id: 123, name: "string" }]);
+
+    useEffect(() => {
+        fetch('/api/user')
+            .then(response => response.json())
+            .then(data => setUser(data));
+    }, []);
+
+    if (!user) {
+        return <div>Loading...</div>;
+    }
+
     return (
-      <div>header</div>
-    )
-  }
-  
+        <div>
+            {
+                user &&
+                <h1>{user[0].id}: {user[0].name}</h1>
+            }
+        </div>
+    );
+}
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,43 @@
+import { http, HttpResponse } from 'msw'
+
+const allPosts = new Map()
+
+// export const handlers = [
+//   // Intercept "GET http://example.com/user" requests...
+//   http.get('http://example.com/user', () => {
+//     // ...and respond to them using this JSON response.
+//     return HttpResponse.json({
+//       id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+//       firstName: 'John',
+//       lastName: 'Maverick',
+//     })
+//   }),
+//   http.get('/posts', () => {
+//     console.log('Captured a "GET /posts" request')
+//     return HttpResponse.json(Array.from(allPosts.values()))
+//   }),
+//   http.post('/posts', () => {
+//     console.log('Captured a "POST /posts" request')
+//   }),
+//   // http.post('/posts', async ({ request }) => {
+//   //   // Read the intercepted request body as JSON.
+//   //   const newPost = await request.json()
+
+//   //   // Push the new post to the map of all posts.
+//   //   allPosts.set(newPost.id, newPost)
+
+//   //   // Don't forget to declare a semantic "201 Created"
+//   //   // response and send back the newly created post!
+//   //   return HttpResponse.json(newPost, { status: 201 })
+//   // }),
+//   http.delete('/posts/:id', ({ params }) => {
+//     console.log(`Captured a "DELETE /posts/${params.id}" request`)
+//   }),
+// ]
+
+export const handlers = [
+  http.get('/api/user', () => {
+    console.log('Handling GET /api/user');
+    return HttpResponse.json([{ id: 1, name: "John" }])
+  }),
+];
