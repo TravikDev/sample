bac908fdb1fa3f0cc7b683d45dd5d96e6ef51a9a
TravikDev <travikdev@gmail.com>
2024-10-06 12:12:06 -0700

    hide logger

--- src/features/clicker/clicker.tsx
+++ src/features/clicker/clicker.tsx
@@ -1210,8 +1210,8 @@ const App: React.FC = () => {
               {/* {JSON.stringify(tg)} */}
               {/* {JSON.stringify(JSON.stringify(consoleLog))} */}
               {/* {JSON.stringify(userProf)} */}
-              {JSON.stringify(data2)}
-              {JSON.stringify(welcomeSalary)}
+              {/* {JSON.stringify(data2)} */}
+              {/* {JSON.stringify(welcomeSalary)} */}
               {/* {JSON.stringify(location)} */}
               {/* //TODO "" */}
               {/* {JSON.stringify(referral)} */}
